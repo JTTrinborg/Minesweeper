60162ecce88ba3e9eee8d71886909ca662254636
Jan Trinborg <jan.t.trinborg@gmail.com>
2025-10-28 01:26:08 +1000

    UPDATE => GameWindow
    Added the MineTiles to the GameWindow and moved frame.setVisible to the end

--- src/main/java/JT/GameWindow.java
+++ src/main/java/JT/GameWindow.java
@@ -3,6 +3,7 @@ package JT;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class GameWindow {
 
     int tileSize = 70;
@@ -17,9 +18,10 @@ public class GameWindow {
     JPanel textPanel = new JPanel();
     JPanel boardPanel = new JPanel();
 
+    MineTile[][] board = new MineTile[numRows][numCols];
+
     //CONSTRUCTOR
     GameWindow(){
-        frame.setVisible(true);
         frame.setSize(boardWith, boardHeight);
         frame.setLocationRelativeTo(null); // Ensures that the board opens in the center of the screen
         frame.setResizable(false);
@@ -41,5 +43,21 @@ public class GameWindow {
         boardPanel.setLayout(new GridLayout(numRows, numCols));
         //boardPanel.setBackground(Color.BLUE);
         frame.add(boardPanel);
+
+        //Adding the MineTiles to the grid
+        for (int r = 0; r < numRows; r++){
+            for (int c = 0; c < numCols; c++){
+                MineTile tile = new MineTile(r, c);
+                board[r][c] = tile;
+
+                tile.setFocusable(false);
+                tile.setMargin(new Insets(0,0,0,0));
+                tile.setFont(new Font("Arial Unicode MS", Font.PLAIN, 45));
+                tile.setText("1");
+                boardPanel.add(tile);
+            }
+        }
+
+        frame.setVisible(true);
     }
 }
